2f7d2f5ed730836aa810d443af29ad6af7371e4b
BWBellairs <BWBellairs@outlook.com>
2019-12-01 20:43:34 +0000

    Day 1 Part 2
    Solved

--- days/1/index.js
+++ days/1/index.js
@@ -1,9 +1,23 @@
 const common = require('../../common.js');
 
-    
+let masses = common.parseNumberFile('./days/1/input.txt');
+
 function part1() {
-    let masses = common.parseNumberFile('./days/1/input.txt');
-    return masses.reduce( (total, mass) => total + (Math.floor(mass / 3) - 2), 0)
+    return masses.reduce( (total, mass) => total + (Math.floor(mass / 3) - 2))
 }
 
-module.exports = [part1]
+function calculateFuel(mass) {
+    let totalFuel = 0;
+    let dependantFuel = (Math.floor(mass / 3) - 2);
+    while (dependantFuel > 0) {
+        totalFuel += dependantFuel;
+        dependantFuel = (Math.floor(dependantFuel / 3) - 2);
+    }
+    return totalFuel
+}   
+
+function part2() {
+    return common.sum( masses.map(mass => calculateFuel(mass) ));
+}
+
+module.exports = [part1, part2]
