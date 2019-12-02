76a2f203e9acdc315329c5e432bd2c5c850ccef5
BWBellairs <BWBellairs@outlook.com>
2019-12-02 22:02:07 +0000

    Day 2 Part 2
    Solved

--- days/2/index.js
+++ days/2/index.js
@@ -1,9 +1,9 @@
 const common = require('../../common.js');
 
 let input = common.readInput('./days/2/input.txt').split(',').map(val => +val);
+let inputCopy = input.slice();
+input.splice(1, 2, 12, 2); // Task set to change some values in input
 function part1() {
-    // Task set to change some values in input
-    input.splice(1, 2, 12, 2);
     let pointerPosition = 0;
     let opcode = input[0];
     
@@ -36,4 +36,16 @@ function part1() {
     return input[0];
 }
 
-module.exports = [part1]
+
+function part2() {
+    for (let i = 0; i < 100; i++) {
+        for (let j = 0; j < 100; j++) {
+            input = inputCopy.slice();
+            input.splice(1, 2, i, j);
+            if (part1() === 19690720) return 100 * i + j;
+        }
+    }
+
+}
+
+module.exports = [part1, part2]
