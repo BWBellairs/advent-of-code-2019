44f7a67f3cf969f82fd182f4da9345554980a4ec
BWBellairs <BWBellairs@outlook.com>
2019-12-08 19:23:41 +0000

    Added ArrayEquals
    Added ArrayEquals method to compare two arrays

--- common.js
+++ common.js
@@ -4,5 +4,15 @@ module.exports = {
     readInput: dir => fs.readFileSync(dir).toString(),
     sum: arr => arr.reduce((a, b) => a + b),
     multiply: arr => arr.reduce((a, b) => a * b),
-    parseNumberFile: dir => fs.readFileSync(dir).toString().split('\n').map(x => +x)
+    parseNumberFile: dir => fs.readFileSync(dir).toString().split('\n').map(x => +x),
+    ArrayEquals: (a, b) => {
+        if (a === undefined) return false;
+        var i = a.length;
+        if (i != b.length) return false;
+        while (i--) {
+            if (typeof a[i] === 'object' && !this.Arrayequals(a[i], b[i])) return false;
+            if (a[i] !== b[i]) return false;
+        }
+        return true;
+    }
 };
